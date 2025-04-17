c6b6b326b2f9a8c56d969fbca95950889c7e07cb
CupOHappiness <nikita.mitrovich@posteo.org>
2025-04-17 14:25:34 -0400

    Score Manager now has Unity Events
    MissThresholdExceeded - Triggered when Miss count exceeds loss threshold.
    SongCompleted - Triggered when sum of all the hits and misses is equal to total notes.
    totalNotes - Needs to be set in the inspector. You can let the song run all the way through and see how many misses there were.

--- Assets/Koreographer/Demos/Rhythm Game Demo/Scripts/Scoring/ScoreManager.cs
+++ Assets/Koreographer/Demos/Rhythm Game Demo/Scripts/Scoring/ScoreManager.cs
@@ -1,32 +1,51 @@
+using System;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Serialization;
 
 namespace SonicBloom.Koreo.Demos
 {
     public class ScoreManager : MonoBehaviour
     {
+        [Header("Lane Tracking")]
         [Tooltip("Reference to the LaneController that this tracker will listen to.")]
         [SerializeField] private LaneController lane;
 
-        private int totalNotes = 0;
-        private int perfectCount = 0;
-        private int goodCount = 0;
-        private int missCount = 0;
+        [Header("Note Tracking")]
+        [Tooltip("Total notes to expect. Set this in the Inspector.")]
+        [Min(1)]
+        [SerializeField] private int totalNotes = 1;
+        [SerializeField]private int _perfectCount;
+        [SerializeField]private int _goodCount;
+        [SerializeField]private int _missCount;
 
+        [Header("Loss Condition")]
         [Tooltip("If Misses exceed this percentage of total notes, the player loses (1-100).")]
         [Range(1, 100)]
         [SerializeField] private int lossThresholdPercent = 40;
 
-        void OnEnable()
+        [Header("Events")]
+        [Tooltip("Triggered when Miss count exceeds loss threshold.")]
+        public UnityEvent MissThresholdExceededUnity;
+        [Tooltip("Triggered when sum of all the hits and misses is equal to total notes.")]
+        public UnityEvent SongCompletedUnity;
+        public event Action MissThresholdExceeded;
+        public event Action SongCompleted;
+
+        
+
+        private void OnEnable()
         {
             if (lane != null)
             {
                 lane.OnFeedback += HandleFeedback;
-                totalNotes = lane.trackedNotes.Count;
-                Debug.Log("Total notes: " + totalNotes);
             }
+            _perfectCount = 0;
+            _goodCount = 0;
+            _missCount = 0;
         }
 
-        void OnDisable()
+        private void OnDisable()
         {
             if (lane != null)
             {
@@ -39,26 +58,48 @@ namespace SonicBloom.Koreo.Demos
             switch (feedback)
             {
                 case NoteObject.Feedback.Perfect:
-                    perfectCount++;
+                    _perfectCount++;
                     break;
                 case NoteObject.Feedback.Good:
-                    goodCount++;
+                    _goodCount++;
                     break;
                 case NoteObject.Feedback.Miss:
-                    missCount++;
+                    _missCount++;
                     break;
             }
 
-           if (totalNotes > 0)
-            {
-                float missRatio = (missCount / (float)totalNotes) * 100f;
+            EvaluateScore();
+        }
 
-                if (missRatio > lossThresholdPercent)
-                {
-                    Debug.Log($"Player lost! Missed {missRatio:F0}% of notes.");
-                    // Optional: Trigger game over logic here
-                }
+        private void EvaluateScore()
+        {
+            int totalHits = _perfectCount + _goodCount + _missCount;
+
+            if (totalHits >= totalNotes)
+            {
+                TriggerGameWon();
+                return;
             }
+
+            float missRatio = (_missCount / (float)totalNotes) * 100f;
+            if (missRatio > lossThresholdPercent)
+            {
+                TriggerGameOver();
+            }
+        }
+
+        private void TriggerGameOver()
+        {
+            Debug.Log($"Player lost! Missed {(_missCount / (float)totalNotes) * 100f:F0}% of notes.");
+            MissThresholdExceeded?.Invoke();
+            MissThresholdExceededUnity?.Invoke();
+        }
+
+        private void TriggerGameWon()
+        {
+            Debug.Log("Player won! All notes evaluated.");
+            SongCompleted?.Invoke();
+            SongCompletedUnity?.Invoke();
         }
     }
 }
